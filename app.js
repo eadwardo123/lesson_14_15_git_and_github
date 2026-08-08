d8743d4c3fbf597b5412f7cf97ac564e654fbce4
eadwardo123 <codingeadwardo@gmail.com>
2026-08-08 17:47:36 +0700

    put some database test

--- app.js
+++ app.js
@@ -87,6 +87,23 @@ app.post("/finance/store", async(req,res) =>{
     }
 })
 
+// database connection test
+app.get("db-test", async(req,res)=>{
+    try{
+        const [rows] = await pool.query("SELECT 1 AS OK")
+        res.json({
+            success:true,
+            data:rows
+        })
+    }catch(err){
+        console.log("Database Error:",err)
+        res.send(err).json({
+            success:false,
+            error:err.code,
+            message:err.message
+        })
+    }
+})
 app.listen(port, ()=>{
     console.log(`server run on http://localhost:${port}`)
 })
