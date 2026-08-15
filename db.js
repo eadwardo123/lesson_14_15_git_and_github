6a3556d56b8429e9cfd54e9b105a617c7ac2cfc1
eadwardo123 <codingeadwardo@gmail.com>
2026-08-15 17:48:06 +0700

    add certificate for vercel bcz this is free and i like freedom but aiven is so stupid so i have to add certificate

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 dotenv.config()
 
@@ -14,10 +16,11 @@ const pool = mysql.createPool({
     queueLimit : 0,
     connectTimeout: 10000,
     ssl:{
-        rejectUnauthorized:false
+        ca: fs.readdirSync(
+        path.join(process.cwd(), 'ca.pem')
+        ),
+        rejectUnauthorized:true
     }
-
-
 })
 
 export default pool
